a5b199fce113490f14a70618da9adfe194b5a280
chepurithrisha <chepuri.thrisha@iitgn.ac.in>
2025-08-04 22:00:24 +0530

    Update py1.py

--- py1.py
+++ py1.py
@@ -8,32 +8,26 @@ def factorial(n):
     for i in range(1,n+1):
         output=output*i
     return output
-
 def permutation(n,r):
     """Calculates the permutation of non-negative integers n,r"""
     final=(factorial(n))/(factorial(n-r))
     return final
-
 def combination(n,r):
     """Calculates the combination of non-negative integers n,r"""
     result=(factorial(n))/((factorial(n-r))*(factorial(r)))
     return result
-
 def main():
     """Takes the input n and r then prints"""
     n=int(input())
     r=int(input())
-    
     if n<0:
         print("Factorial of this number is undefined")
     else:
         print("The Factorial of this number " ,factorial(n))
-    
     if r<0 or n<r or n<0:
         print("Permutation and Combination both does not exist")
     else:
         print("The Permutation of n and r is",permutation(n,r))
         print("The combination of n and r is",combination(n,r))
-
 if __name__ == "__main__":
     main()
